5425b5724eb9622e0e06e29158aa2d905b3f9f8c
jessicalyruiz <jessicalyr@gmail.com>
2023-06-15 19:45:49 -0500

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Hello" nombre="Jessicaly"/>
+  <HelloWorld msg="Hello Jessicaly2"/>
+  <Componente1/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Componente1 from './components/Componente1.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Componente1,
   }
 }
 </script>
---
+++ src/components/Componente1.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Este es un componente</h1>
+</template>
+
+<script>
+export default {
+    name:'comp1',
+}
+</script>
+
+<style>
+
+</style>
