964518d57e48dbea0a8e34c49b6c83e08317e6d3
jessicalyruiz <jessicalyr@gmail.com>
2023-06-22 19:42:23 -0500

    Comsumir Api pregunta

--- src/components/Pregunta.vue
+++ src/components/Pregunta.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="caracola">
-    <h1>Caracola Magica</h1>
-    <img src="https://via.placeholder.com/250" alt="No se muestra" />
+    <h1>Caracola Mágica</h1>
+    <div class=imagen></div>
+    <img :src="img" alt="No se muestra" />
+    
+   <div class="contenedor">
     <input v-model="pregunta" type="text" placeholder="Hazme una pregunta" />
-    <p>recuerda terminar con un enter</p>
+    <p>recuerda terminar con un ?</p>
 
     <div>
-        <h2>{{pregunta}}</h2>
-        <h1>{{respuesta}}</h1>
+      <h2>{{ pregunta }}</h2>
+      <h1>{{ respuesta }}</h1>
+    </div>
+
     </div>
   </div>
 </template>
@@ -20,9 +25,31 @@ export default {
     return {
       pregunta: "",
       respuesta: "",
+      img:'https://i1.ytimg.com/vi/Tb_DY0DVCn4/mqdefault.jpg',
     };
   },
-  methods: {},
+  methods: {
+    async consumirAPI(){
+      console.log(await fetch('https://yesno.wtf/api').then(r=>r.json()))
+      return await fetch('https://yesno.wtf/api').then(r=>r.json())
+     
+    }
+
+  },
+  watch: {
+    async pregunta(value, oldValue){
+      console.log('value: '+value)
+      console.log('oldValue: '+oldValue)
+      if(value.includes('?')){
+        console.log('consumo el  API')
+       const {answer, image}= await this.consumirAPI()
+       this.respuesta=answer
+       this.img=image
+       console.log(this.respuesta)
+
+      }
+    }
+  },
 };
 </script>
 
@@ -34,5 +61,36 @@ export default {
 .caracola {
   display: grid;
   align-items: center;
+  justify-content: center;
+}
+img, imagen{
+
+  height: 100vh;
+  width: 100vw;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+}
+.contenedor{
+position: relative;
+}
+.imagen{
+  background-color: rgba(0,0 ,0, 0.2);
+}
+input{
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 10px;
+  border: none;
+}
+p,h1,h2{
+  color: white;
+}
+
+p{
+  font-size: 20px;
+  margin-top: 0px;
 }
 </style>
